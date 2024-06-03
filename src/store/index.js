89b978cc26f0021faea565cb59aacfc881910d8f
rohanchandane <rohan.chandane@gmail.com>
2024-06-03 01:22:47 +0100

    added redux devtool for debugging store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { thunk } from  'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 import todoReducer from "./reducer/todo";
 
-const store = createStore(combineReducers({todoReducer}), applyMiddleware(thunk));
+const store = createStore(combineReducers({todoReducer}), composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
